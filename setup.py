1db08a7a1bed44a56f61066fce1436a26ecfc3ed
Alex Tomkins <tomkins@darkzone.net>
2014-05-21 12:48:49 +0100

    Updated setup.py for setuptools, new Python versions

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='blanc-basic-assets',
     version='0.2.1',
     description='Blanc Basic Assets for Django',
     long_description=open('README.rst').read(),
-    url='http://www.blanctools.com/',
+    url='https://github.com/blancltd/blanc-basic-assets',
     maintainer='Alex Tomkins',
-    maintainer_email='alex@hawkz.com',
+    maintainer_email='alex@blanc.ltd.uk',
     platforms=['any'],
-    packages=[
-        'blanc_basic_assets',
-    ],
+    packages=find_packages(exclude=['tests']),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
-    license='BSD-2',
+    license='BSD',
 )
