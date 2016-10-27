07358dcef7a5a15f3de3f54b2db69833943d3936
Thomas Lundqvist <biljettshop@users.noreply.github.com>
2016-10-27 14:48:17 +0200

    Update fields.py

--- blanc_basic_assets/fields.py
+++ blanc_basic_assets/fields.py
@@ -35,20 +35,9 @@ class GroupedModelChoiceIterator(ModelChoiceIterator):
         if self.field.empty_label is not None:
             yield ('', self.field.empty_label)
 
-        if self.field.cache_choices:
-            if self.field.choice_cache is None:
-                self.field.choice_cache = [
-                    (self.field.group_label(group), [self.choice(ch) for ch in choices])
-                    for group, choices in groupby(self.queryset.all(),
-                                                  key=lambda row: getattr(row, self.field.group_by_field))
-                ]
-
-            for choice in self.field.choice_cache:
-                yield choice
-        else:
-            for group, choices in groupby(self.queryset.all(),
-                                          key=lambda row: getattr(row, self.field.group_by_field)):
-                yield (self.field.group_label(group), [self.choice(ch) for ch in choices])
+        for group, choices in groupby(self.queryset.all(),
+                                      key=lambda row: getattr(row, self.field.group_by_field)):
+            yield (self.field.group_label(group), [self.choice(ch) for ch in choices])
 
 
 class AssetForeignKey(models.ForeignKey):
